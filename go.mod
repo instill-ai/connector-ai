c7d21692e732a1594b74d893ccdc6d342222618a
Praharsh Jain <praharshjain@users.noreply.github.com>
2023-06-29 12:06:57 +0530

    feat: Support using Stability AI API as a model connector (#1)
    Because
    
    - we want to integrate with 3rd party models/APIs and do not have
    connector for models
    
    This commit
    
    - introduces the concept of connector for models and code for Stability
    AI integration
    
    ---------
    
    Co-authored-by: Praharsh Jain <praharsh.jain@grabtaxi.com>
    Co-authored-by: Chang, Hui-Tang <huitang.chang@instill.tech>

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/instill-ai/connector-ai
+
+go 1.20
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/instill-ai/connector v0.0.0-20230628152420-4adf6bedec28
+	github.com/instill-ai/protogen-go v0.3.3-alpha.0.20230628145744-8bd74278dff2
+	github.com/stretchr/testify v1.8.4
+	go.uber.org/zap v1.24.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20220107163113-42d7afdf6368 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/stabilityai/text_to_image.go
@@ -0,0 +1,56 @@
+package stabilityai
+
+import "fmt"
+
+const (
+	successFinishReason = "SUCCESS"
+)
+
+// TextToImageReq represents the request body for text-to-image API
+type TextToImageReq struct {
+	TextPrompts        []TextPrompt `json:"text_prompts" om:"texts[:]"`
+	CFGScale           float64      `json:"cfg_scale,omitempty" om:"metadata.cfg_scale"`
+	ClipGuidancePreset string       `json:"clip_guidance_preset,omitempty" om:"metadata.clip_guidance_preset"`
+	Sampler            string       `json:"sampler,omitempty" om:"metadata.sampler"`
+	Samples            uint32       `json:"samples,omitempty" om:"metadata.samples"`
+	Seed               uint32       `json:"seed,omitempty" om:"metadata.seed"`
+	Steps              uint32       `json:"steps,omitempty" om:"metadata.steps"`
+	StylePreset        string       `json:"style_preset,omitempty" om:"metadata.style_preset"`
+	Height             uint32       `json:"height,omitempty" om:"metadata.height"`
+	Width              uint32       `json:"width,omitempty" om:"metadata.width"`
+}
+
+// TextPrompt holds a prompt's text and its weight.
+type TextPrompt struct {
+	Text   string  `json:"text" om:"."`
+	Weight float32 `json:"weight"`
+}
+
+// Image represents a single image
+type Image struct {
+	Base64       string `json:"base64"`
+	Seed         uint32 `json:"seed"`
+	FinishReason string `json:"finishReason"`
+}
+
+// ImageTaskRes represents the response body for text-to-image API
+type ImageTaskRes struct {
+	Images []Image `json:"artifacts"`
+}
+
+// GenerateImageFromText makes a call to the text-to-image API from Stability AI.
+// https://platform.stability.ai/rest-api#tag/v1generation/operation/textToImage
+func (c *Client) GenerateImageFromText(params TextToImageReq, engine string) (results []Image, err error) {
+	var resp ImageTaskRes
+	if engine == "" {
+		return nil, fmt.Errorf("no engine selected")
+	}
+	textToImageURL := host + "/v1/generation/" + engine + "/text-to-image"
+	err = c.sendReq(textToImageURL, "POST", params, &resp)
+	for _, i := range resp.Images {
+		if i.FinishReason == successFinishReason {
+			results = append(results, i)
+		}
+	}
+	return
+}
