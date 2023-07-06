64a3f2f9f7fe06919a02468f50d4da3b4245056e
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-07-07 03:19:35 +0800

    chore: update object detection output

--- pkg/instill/object_detection.go
+++ pkg/instill/object_detection.go
@@ -14,20 +14,26 @@ func (c *Connection) executeObjectDetection(model *Model, inputs []*connectorPB.
 	if len(inputs) <= 0 {
 		return nil, fmt.Errorf("invalid input: %v for model: %s", inputs, model.Name)
 	}
-	dataPayload := inputs[0]
-	if len(dataPayload.Images) <= 0 {
-		return nil, fmt.Errorf("invalid input: %v for model: %s", *dataPayload, model.Name)
+
+	tasklInputs := []*modelPB.TaskInput{}
+	for idx := range inputs {
+		dataPayload := inputs[idx]
+		if len(dataPayload.Images) <= 0 {
+			return nil, fmt.Errorf("invalid input: %v for model: %s", *dataPayload, model.Name)
+		}
+		base64Str, err := encodeToBase64(dataPayload.Images[idx])
+		if err != nil {
+			return nil, fmt.Errorf("invalid image string: %v for model: %s", dataPayload.Images[idx], model.Name)
+		}
+		modelInput := &modelPB.TaskInput_Detection{
+			Detection: &modelPB.DetectionInput{Type: &modelPB.DetectionInput_ImageBase64{ImageBase64: base64Str}},
+		}
+		tasklInputs = append(tasklInputs, &modelPB.TaskInput{Input: modelInput})
 	}
-	base64Str, err := encodeToBase64(dataPayload.Images[0])
-	if err != nil {
-		return nil, fmt.Errorf("invalid image string: %v for model: %s", dataPayload.Images[0], model.Name)
-	}
-	modelInput := &modelPB.TaskInput_Detection{
-		Detection: &modelPB.DetectionInput{Type: &modelPB.DetectionInput_ImageBase64{ImageBase64: base64Str}},
-	}
+
 	req := modelPB.TriggerModelRequest{
 		Name:       model.Name,
-		TaskInputs: []*modelPB.TaskInput{{Input: modelInput}},
+		TaskInputs: tasklInputs,
 	}
 	if c.client == nil || c.client.GRPCClient == nil {
 		return nil, fmt.Errorf("client not setup: %v", c.client)
@@ -36,43 +42,53 @@ func (c *Connection) executeObjectDetection(model *Model, inputs []*connectorPB.
 	if err != nil || res == nil {
 		return nil, err
 	}
-	output := res.GetTaskOutputs()
-	if len(output) <= 0 {
-		return nil, fmt.Errorf("invalid output: %v for model: %s", output, model.Name)
+
+	taskOutputs := res.GetTaskOutputs()
+	if len(taskOutputs) <= 0 {
+		return nil, fmt.Errorf("invalid output: %v for model: %s", taskOutputs, model.Name)
 	}
-	objDetectionOutput := output[0].GetDetection()
-	if objDetectionOutput == nil {
-		return nil, fmt.Errorf("invalid output: %v for model: %s", objDetectionOutput, model.Name)
-	}
-	values := make([]*structpb.Value, 0, len(objDetectionOutput.Objects))
-	for _, o := range objDetectionOutput.Objects {
-		obj := &structpb.Value{
-			Kind: &structpb.Value_StructValue{
-				StructValue: &structpb.Struct{
-					Fields: map[string]*structpb.Value{
-						"category": {Kind: &structpb.Value_StringValue{StringValue: o.Category}},
-						"score":    {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.Score)}},
-						"bounding_box": {Kind: &structpb.Value_StructValue{
-							StructValue: &structpb.Struct{
-								Fields: map[string]*structpb.Value{
-									"top":    {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Top)}},
-									"left":   {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Left)}},
-									"width":  {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Width)}},
-									"height": {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Height)}},
+
+	outputs := []*connectorPB.DataPayload{}
+	for idx := range inputs {
+		objDetectionOutput := taskOutputs[idx].GetDetection()
+		if objDetectionOutput == nil {
+			return nil, fmt.Errorf("invalid output: %v for model: %s", objDetectionOutput, model.Name)
+		}
+		values := make([]*structpb.Value, 0, len(objDetectionOutput.Objects))
+		for _, o := range objDetectionOutput.Objects {
+			obj := &structpb.Value{
+				Kind: &structpb.Value_StructValue{
+					StructValue: &structpb.Struct{
+						Fields: map[string]*structpb.Value{
+							"category": {Kind: &structpb.Value_StringValue{StringValue: o.Category}},
+							"score":    {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.Score)}},
+							"bounding_box": {Kind: &structpb.Value_StructValue{
+								StructValue: &structpb.Struct{
+									Fields: map[string]*structpb.Value{
+										"top":    {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Top)}},
+										"left":   {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Left)}},
+										"width":  {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Width)}},
+										"height": {Kind: &structpb.Value_NumberValue{NumberValue: float64(o.BoundingBox.Height)}},
+									},
 								},
 							},
-						},
+							},
 						},
 					},
 				},
-			},
+			}
+			values = append(values, obj)
 		}
-		values = append(values, obj)
+		outputs = append(outputs, &connectorPB.DataPayload{
+			DataMappingIndex: inputs[idx].DataMappingIndex,
+			StructuredData: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"objects": {Kind: &structpb.Value_ListValue{ListValue: &structpb.ListValue{Values: values}}},
+				},
+			},
+		})
+
 	}
-	inputs[0].StructuredData = &structpb.Struct{
-		Fields: map[string]*structpb.Value{
-			"objects": {Kind: &structpb.Value_ListValue{ListValue: &structpb.ListValue{Values: values}}},
-		},
-	}
-	return inputs, nil
+
+	return outputs, nil
 }
