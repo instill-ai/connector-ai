e55e7500557f04474ac00955592f16806e9d47f5
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-07-07 04:45:03 +0800

    chore: fix batch inference problem (#10)
    Because
    
    - the current implementation only process first data
    
    This commit
    
    - process all data in the inputs list
    - batch inference

--- pkg/instill/text_generation.go
+++ pkg/instill/text_generation.go
@@ -12,45 +12,56 @@ func (c *Connection) executeTextGeneration(model *Model, inputs []*connectorPB.D
 	if len(inputs) <= 0 {
 		return nil, fmt.Errorf("invalid input: %v for model: %s", inputs, model.Name)
 	}
-	dataPayload := inputs[0]
-	if len(dataPayload.Texts) <= 0 {
-		return nil, fmt.Errorf("invalid input: %v for model: %s", *dataPayload, model.Name)
-	}
-	seed := int64(dataPayload.GetMetadata().GetFields()["seed"].GetNumberValue())
-	outputLen := int64(dataPayload.GetMetadata().GetFields()["output_len"].GetNumberValue())
-	badWords := dataPayload.GetMetadata().GetFields()["bad_words"].GetStringValue()
-	stopWords := dataPayload.GetMetadata().GetFields()["stop_words"].GetStringValue()
-	topK := int64(dataPayload.GetMetadata().GetFields()["top_k"].GetNumberValue())
 
-	modelInput := &modelPB.TaskInput_TextGeneration{
-		TextGeneration: &modelPB.TextGenerationInput{
-			Prompt:        dataPayload.Texts[0],
-			OutputLen:     &outputLen,
-			BadWordsList:  &badWords,
-			StopWordsList: &stopWords,
-			Topk:          &topK,
-			Seed:          &seed,
-		},
+	outputs := []*connectorPB.DataPayload{}
+
+	for idx := range inputs {
+		dataPayload := inputs[idx]
+		if len(dataPayload.Texts) <= 0 {
+			return nil, fmt.Errorf("invalid input: %v for model: %s", *dataPayload, model.Name)
+		}
+		seed := int64(dataPayload.GetMetadata().GetFields()["seed"].GetNumberValue())
+		outputLen := int64(dataPayload.GetMetadata().GetFields()["output_len"].GetNumberValue())
+		badWords := dataPayload.GetMetadata().GetFields()["bad_words"].GetStringValue()
+		stopWords := dataPayload.GetMetadata().GetFields()["stop_words"].GetStringValue()
+		topK := int64(dataPayload.GetMetadata().GetFields()["top_k"].GetNumberValue())
+
+		taskInput := &modelPB.TaskInput_TextGeneration{
+			TextGeneration: &modelPB.TextGenerationInput{
+				Prompt:        dataPayload.Texts[0],
+				OutputLen:     &outputLen,
+				BadWordsList:  &badWords,
+				StopWordsList: &stopWords,
+				Topk:          &topK,
+				Seed:          &seed,
+			},
+		}
+
+		// only support batch 1
+		req := modelPB.TriggerModelRequest{
+			Name:       model.Name,
+			TaskInputs: []*modelPB.TaskInput{{Input: taskInput}},
+		}
+		if c.client == nil || c.client.GRPCClient == nil {
+			return nil, fmt.Errorf("client not setup: %v", c.client)
+		}
+		res, err := c.client.GRPCClient.TriggerModel(context.Background(), &req)
+		if err != nil || res == nil {
+			return nil, err
+		}
+		taskOutputs := res.GetTaskOutputs()
+		if len(taskOutputs) <= 0 {
+			return nil, fmt.Errorf("invalid output: %v for model: %s", taskOutputs, model.Name)
+		}
+
+		textGenOutput := taskOutputs[0].GetTextGeneration()
+		if textGenOutput == nil || len(textGenOutput.GetText()) <= 0 {
+			return nil, fmt.Errorf("invalid output: %v for model: %s", textGenOutput, model.Name)
+		}
+		outputs = append(outputs, &connectorPB.DataPayload{
+			DataMappingIndex: inputs[idx].DataMappingIndex,
+			Texts:            []string{textGenOutput.GetText()},
+		})
 	}
-	req := modelPB.TriggerModelRequest{
-		Name:       model.Name,
-		TaskInputs: []*modelPB.TaskInput{{Input: modelInput}},
-	}
-	if c.client == nil || c.client.GRPCClient == nil {
-		return nil, fmt.Errorf("client not setup: %v", c.client)
-	}
-	res, err := c.client.GRPCClient.TriggerModel(context.Background(), &req)
-	if err != nil || res == nil {
-		return nil, err
-	}
-	output := res.GetTaskOutputs()
-	if len(output) <= 0 {
-		return nil, fmt.Errorf("invalid output: %v for model: %s", output, model.Name)
-	}
-	textGenOutput := output[0].GetTextGeneration()
-	if textGenOutput == nil || len(textGenOutput.GetText()) <= 0 {
-		return nil, fmt.Errorf("invalid output: %v for model: %s", textGenOutput, model.Name)
-	}
-	inputs[0].Texts = []string{textGenOutput.GetText()}
-	return inputs, nil
+	return outputs, nil
 }
